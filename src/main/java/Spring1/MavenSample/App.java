8604f912eb893f7b84c40c4bb3160f89a4c303e2
Dipendra Kawadkar <dipendra7190kawadkar@gmail.com>
2024-08-26 23:18:25 +0530

    Add some filr

---
+++ src/main/java/Spring1/MavenSample/App.java
@@ -0,0 +1,13 @@
+package Spring1.MavenSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
